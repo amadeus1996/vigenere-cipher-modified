9ef192341c09d1319f33d0a0eff32d3994a166f2
amadeus1996 <firstplayer484@protonmail.com>
2024-09-27 22:20:33 +0000

    refined code for encryption

--- encrypt.s
+++ encrypt.s
@@ -3,48 +3,64 @@
 .data
    string: .space 64
    key: .space 64
-   stringBits: .space 448
-   keyBits: .space 448
    n: .space 4
    m: .space 4
+   stringBits: .space 448
+   keyBits: .space 448
    vowels: .space 6
    formatString1: .asciz "%s"
    formatString2: .asciz "%[^\n]"
+   formatInt: .asciz "%d\n"
    formatStringPrint: .asciz "%s\n"
-   formatInt: .asciz "%d "
    formatChar: .asciz "%c"
+   
 .text
-.global main
-main:
-   # INITIALIZING THE VOWELS ARRAY
-   #lea vowels, %edi
 
+.global main
+
+main:
+   # READ INPUT - 0 FOR ENCRYPTION, 1 FOR DECRYPTION
+   pushl $n
+   pushl $formatInt
+   call scanf
+   addl $8, %esp
+   
+   cmpb $1, n
+   je main_decrypt
+   
+main_encrypt:
    # READING THE TWO INPUT STRINGS
-   pushl $string
+   leal string, %eax
+   pushl %eax
    pushl $formatString2
    call scanf
    addl $8, %esp
-   pushl $key
+   
+   leal key, %eax
+   pushl %eax
    pushl $formatString1
    call scanf
    addl $8, %esp
    
    # GETTING THE LENGTHS OF THE TEXT AND ENCRYPTION KEY
-   pushl $string
+   leal string, %esi
+   leal key, %edi
+   
+   pushl %esi
    call strlen
    addl $4, %esp
-   movl %eax, n
-   pushl $key
+   
+   movb %al, n
+   
+   pushl %edi
    call strlen
    addl $4, %esp
-   movl %eax, m
+   movb %al, m
    
    # 2 CASES:
    # a) n <= m (LENGTH OF TEXT <= LENGTH OF ENCRYPTION KEY)
-   # b) n > m (LENGTH OF TEXT > LENGTH OF ENCRYPTION KEY)
-   lea string, %esi
-   lea key, %edi
-   cmpl %eax, n
+   # b) n > m (LENGTH OF TEXT > LENGTH OF ENCRYPTION KEY) => REPEAT THE TEXT
+   cmpb %al, n
    jbe pre_vigenere
    
    xorl %ecx, %ecx
@@ -52,20 +68,19 @@ main:
    
    # ADD CHARACTERS TO THE KEY SO IT MATCHES THE TEXT'S LENGTH 
    add_chars:
-      cmpl n, %eax
+      cmpb n, %al
       je pre_vigenere
       
       movb (%edi, %ecx, 1), %bl
       movb %bl, (%edi, %eax, 1)
-      incl %ecx
+      incb %cl
       
-      cmpl m, %ecx
+      cmpb m, %cl
       jne add_chars_final
-      xorl %ecx, %ecx
-      jmp add_chars_final
+      xorb %cl, %cl
          
       add_chars_final:
-         incl %eax
+         incb %al
          jmp add_chars
    
 # PRINT THE ENCRYPTION KEY FOR DEBUGGING PURPOSES
@@ -78,87 +93,86 @@ pre_vigenere:
    addl $12, %esp
    
    xorl %eax, %eax
-   xorl %ebx, %ebx
+   xorb %bl, %bl
    xorl %ecx, %ecx
    
 # APPLY VIGENERE CIPHER TO THE ORIGINAL TEXT   
 vigenere:
-   cmpl %ecx, n
+   cmpb n, %cl
    je pre_update_bits
    
    # CHECK IF IT'S A LETTER (IF NOT, DON'T ENCRYPT)
    # 65 <= char <= 90 || 97 <= char <= 122
    movb (%esi, %ecx, 1), %al 
-   cmpl $65, %eax
+   cmpb $65, %al
    jb vigenere_final
-   cmpl $90, %eax
+   cmpb $90, %al
    jbe vigenere_cont_1
-   cmpl $97, %eax
+   cmpb $97, %al
    jb vigenere_final
-   cmpl $122, %eax
+   cmpb $122, %al
    ja vigenere_final
    
-   # SOME COMMON OPERATIONS. CHECK IF THE CURRENT CHAR OF THE KEY
-   # IS A LOWERCASE OR UPPERCASE
+   # IT IS A LETTER. CHECK IF THE CURRENT CHAR OF THE KEY IS LOWERCASE OR UPPERCASE
    vigenere_cont_1:
       movb (%edi, %ecx, 1), %bl
-      xorl %edx, %edx
-      subl $65, %ebx
-      cmpl $31, %ebx
+      xorl %edx, %edx # PREPARING FOR DIVISION
+      subb $65, %bl
+      cmpb $31, %bl
       jbe vigenere_cont_2
-      subl $32, %ebx
+      subb $32, %bl
    
    vigenere_cont_2:
       # CHECK IF THE CURRENT TEXT CHAR IS A LOWERCASE OR UPPERCASE
-      cmpl $97, %eax
+      cmpb $97, %al
       jb vigenere_uppercase
    
    vigenere_lowercase:
-      subl $97, %eax
-      addl %ebx, %eax
-      movl $26, %ebx
+      subb $97, %al
+      addb %bl, %al
+      movb $26, %bl
       divl %ebx
-      addl $97, %edx
-      movl %edx, %eax
+      addb $97, %dl
+      movb %dl, %al
       jmp vigenere_final
 
    vigenere_uppercase:
-      subl $65, %eax
-      addl %ebx, %eax
-      movl $26, %ebx
+      subb $65, %al
+      addb %bl, %al
+      movb $26, %bl
       divl %ebx
-      addl $65, %edx
-      movl %edx, %eax
+      addb $65, %dl
+      movb %dl, %al
    
    vigenere_final:
       movb %al, (%esi, %ecx, 1)
-      incl %ecx
+      incb %cl
       jmp vigenere
    
 pre_update_bits:
-   xorl %ecx, %ecx
-   xorl %eax, %eax
+   xorb %cl, %cl
+   xorb %al, %al
    
 update_bits:
-   # FOR BOTH STRINGS (ENCRYPTED TEXT AND KEY): STORE THE VALUES OF
-   # EVERY LETTER IN BINARY. ENCRYPT THE BITS USING THE RULES
-   cmpl %ecx, n
+   # PERFORM OPERATIONS ON THE BITS FOR FURTHER ENCRYPTION
+   cmpb n, %cl
    je pre_print_encrypted_string
    
-   # CHECK IF IT'S A LETTER. IF NOT, DON'T ENCRYPT/CALCULATE BITS.
+   # CHECK IF THE CURRENT TEXT CHAR IS A LETTER. 
+   # IF NOT, SKIP IT
    xorl %ebx, %ebx
    movb (%esi, %ecx, 1), %bl
-   cmpl $65, %ebx
+   cmpb $65, %bl
    jb update_bits_final
-   cmpl $90, %ebx
+   cmpb $90, %bl
    jbe update_bits_cont
-   cmpl $97, %ebx
+   cmpb $97, %bl
    jb update_bits_final
-   cmpl $122, %ebx
+   cmpb $122, %bl
    ja update_bits_final
    
-   # IT IS A LETTER. GET THE LETTER OF THE KEY AND START WORKING ON BITS
-   # IDEA: CHECK IF THE CURRENT CASE IS VV / VC / CV / CC.
+   # IT IS A LETTER. START ENCRYPTING THE BITS.
+   # CHECK IF THE CURRENT CASE IS VV / VC / CV / CC.
    # IF IT'S VV OR CC, CALL POWERS_OF_2 AND THEN ENCRYPT BITS.
    # IF IT'S CV OR VC, IT'S REDUNDANT TO CALL POWERS_OF_2. JUST ADD $7 TO %EAX (POWERS_OF_2 WOULD HAVE DONE THAT) AND THEN ENCRYPT BITS.
    # IT'S BECAUSE OF THE ENCRYPTION RULES. READ THE ALGORITHM IDEA.
@@ -168,37 +182,38 @@ update_bits:
      
       pushl %ecx # MUST KEEP ECX'S VALUE
       
+      # GET THE CURRENT VOWEL-CONSONANT CASE (VV, VC, CV, CC)
+      # PUSH %EAX TO SAVE THE VALUE FOR THE FUTURE
       pushl %edx
       pushl %ebx
-      pushl %eax # MUST KEEP THE SIZE OF THE BITS ARRAYS (STORED IN EAX)
+      pushl %eax
       call vowel_or_consonant
       
       # %EAX CONTAINS THE CASE CODE (0-3)
       # 0 = VV, 3 = CC => CALCULATE THE BITS AND PERFORM `AND` OPERATION
       # 1 = VC, 2 = CV => DON'T REDUNDANTLY CALCULATE THE BITS
-      # CHECK THE CASE RULES TO UNDERSTAND
-      debug:
-         movl %eax, %ecx # %ECX WILL NOW STORE THE CASE CODE
-         popl %eax # RESTORE THE SIZE OF THE BIT-ARRAYS
-         pushl %ecx # LOAD THE CASE CODE ONTO THE STACK
+      
+      movl %eax, %ecx # %ECX WILL NOW STORE THE CASE CODE
+      popl %eax       # RESTORE THE SIZE OF THE BIT-ARRAYS
+      pushl %ecx      # LOAD THE CASE CODE ONTO THE STACK
          
-         lea stringBits, %esi
-         lea keyBits, %edi
+      leal stringBits, %esi
+      leal keyBits, %edi
          
-         cmpl $0, %ecx
-         je call_powers_of_2
-         cmpl $3, %ecx
-         je call_powers_of_2
+      cmpb $0, %cl
+      je call_powers_of_2
+      cmpb $3, %cl
+      je call_powers_of_2
          
-         # BECAUSE THE CASE IS NOT 0 OR 3 (VV OR CC), IT WILL SKIP POW_OF_2
-         # THAT MEANS THE BIT-ARRAY'S SIZE WON'T INCREASE 
-         # WE HAVE TO ADD 7 TO THE LENGTH OURSELVES
-         addl $7, %eax
-         jmp call_encrypt_bits
+      # BECAUSE THE CASE IS NOT 0 OR 3 (VV OR CC), IT WILL SKIP POW_OF_2
+      # THAT MEANS THE SIZE OF THE BIT-ARRAYS WON'T INCREASE 
+      # WE HAVE TO MANUALLY ADD 7 TO THE SIZE
+      addw $7, %ax
+      jmp call_encrypt_bits
       
       call_powers_of_2:
          # WE WILL NOW PUSH %EAX. THE STACK BECOMES THE FOLLOWING:
-         # 8(%ebp) = %eax (CURRENT SIZE OF THE BITS ARRAY)
+         # 8(%ebp)  = %eax (CURRENT SIZE OF THE BITS ARRAY)
          # 12(%ebp) = %ecx (CURRENT CASE CODE)
          # 16(%ebp) = %ebx (CURRENT TEXT CHARACTER)
          # 20(%ebp) = %edx (CURRENT KEY CHARACTER)
@@ -221,18 +236,17 @@ update_bits:
          popl %ecx      # RESTORE THE COUNTER 
    
    update_bits_final:
-      lea string, %esi
-      lea key, %edi
-      incl %ecx
+      leal string, %esi
+      leal key, %edi
+      incb %cl
       jmp update_bits
       
 pre_print_encrypted_string:
    # %eax = NUMBER OF BITS IN THE ARRAY
    xorl %ecx, %ecx
    xorl %edx, %edx
-   xorl %ebx, %ebx
    movl $64, m
-   movl $0, n
+   movb $0, n
 
 print_encrypted_string:
    # CHECK IF THE CURRENT CHARACTER OF THE STRING IS A LETTER OR NOT
@@ -240,39 +254,40 @@ print_encrypted_string:
    movl n, %ebx
    movb (%esi, %ebx, 1), %bl
       
-   cmpl $65, %ebx
+   cmpb $65, %bl
    jb print_encrypted_string_pre_final
-   cmpl $90, %ebx
+   cmpb $90, %bl
    jbe print_encrypted_string_pre_for
-   cmpl $97, %ebx
+   cmpb $97, %bl
    jb print_encrypted_string_pre_final
-   cmpl $122, %ebx
+   cmpb $122, %bl
    ja print_encrypted_string_pre_final
    
    # IT IS A LETTER, SO RESET THE VALUE IN %EBX
+   # %EBX WILL BE USED TO TURN IT FROM BINARY TO TEXT
    print_encrypted_string_pre_for:
       xorl %ebx, %ebx
-      lea stringBits, %esi
+      leal stringBits, %esi
    
    print_encrypted_string_for:
-      cmpl $7, %ecx
+      cmpb $7, %cl
       je print_encrypted_string_final
       
       # IF THE CURRENT BIT IS 0, DON'T ADD THAT POWER OF 2
       cmpb $0, (%esi, %edx, 1)
       je print_encrypted_string_for_cont
       
-      addl m, %ebx
+      addb m, %bl
       
       print_encrypted_string_for_cont:
-         incl %ecx
-         incl %edx
-         shrl $1, m
+         incb %cl
+         incw %dx
+         shrb $1, m
          jmp print_encrypted_string_for
          
    print_encrypted_string_pre_final:
       # IF THE CHARACTER IS NOT A LETTER, PRINT THE ORIGINAL CHARACTER
-      movl n, %ebx
+      movb n, %bl
       movb (%esi, %ebx, 1), %bl
       
    print_encrypted_string_final:
@@ -289,14 +304,13 @@ print_encrypted_string:
       popl %edx
       popl %eax
    
-      lea string, %esi
-      movl $64, m
-      incl n
-      xorl %ecx, %ecx
-      xorl %ebx, %ebx
-      cmpl %edx, %eax
+      leal string, %esi
+      movb $64, m
+      incb n
+      xorb %cl, %cl
+      xorb %bl, %bl
+      cmpw %dx, %ax
       je exit
-      
       jmp print_encrypted_string
    
 vowel_or_consonant:
@@ -309,73 +323,73 @@ vowel_or_consonant:
    
    movl 12(%ebp), %eax
    movl 16(%ebp), %ebx
-   cmpl $97, %eax
+   cmpb $97, %al
    jb vowel_or_consonant_XX
-   subl $32, %eax
+   subb $32, %al
    
    vowel_or_consonant_XX:
       # IS THE CURRENT TEXT CHAR A VOWEL OR A CONSONANT?
       # EAX IS CURRENTLY LOWERCASE. MAKE EBX LOWERCASE AS WELL
-      cmpl $97, %ebx
+      cmpb $97, %bl
       jb vowel_or_consonant_XX_cont
-      subl $32, %ebx
+      subb $32, %bl
       
       vowel_or_consonant_XX_cont:
-         cmpl $65, %eax # 'A' = 65
+         cmpb $65, %al # 'A' = 65
          je vowel_or_consonant_VX
-         cmpl $69, %eax # 'E' = 69
+         cmpb $69, %al # 'E' = 69
          je vowel_or_consonant_VX
-         cmpl $73, %eax # 'I' = 73
+         cmpb $73, %al # 'I' = 73
          je vowel_or_consonant_VX
-         cmpl $79, %eax # 'O' = 79
+         cmpb $79, %al # 'O' = 79
          je vowel_or_consonant_VX
-         cmpl $85, %eax # 'U' = 85
+         cmpb $85, %al # 'U' = 85
          jne vowel_or_consonant_CX
       
    vowel_or_consonant_VX:
       # IS THE CURRENT KEY CHAR A VOWEL OR A CONSONANT?
-      cmpl $65, %ebx # A
+      cmpb $65, %bl # A
       je vowel_or_consonant_VV
-      cmpl $69, %ebx # E
+      cmpb $69, %bl # E
       je vowel_or_consonant_VV
-      cmpl $73, %ebx # 
+      cmpb $73, %bl # I
       je vowel_or_consonant_VV
-      cmpl $79, %ebx # O
+      cmpb $79, %bl # O
       je vowel_or_consonant_VV
-      cmpl $85, %ebx # U
+      cmpb $85, %bl # U
       jne vowel_or_consonant_VC
          
    vowel_or_consonant_VV:
       # CASE VV = CODE 0
-      xorl %eax, %eax
+      xorb %al, %al
       jmp vowel_or_consonant_final
    
    vowel_or_consonant_CX:
       # IS THE CURRENT KEY CHAR A VOWEL OR A CONSONANT?
-      cmpl $65, %ebx
+      cmpb $65, %bl
       je vowel_or_consonant_CV
-      cmpl $69, %ebx
+      cmpb $69, %bl
       je vowel_or_consonant_CV
-      cmpl $73, %ebx
+      cmpb $73, %bl
       je vowel_or_consonant_CV
-      cmpl $79, %ebx
+      cmpb $79, %bl
       je vowel_or_consonant_CV
-      cmpl $85, %ebx
+      cmpb $85, %bl
       je vowel_or_consonant_CV
       
    vowel_or_consonant_CC:
       # CASE CC = CODE 3
-      movl $3, %eax
+      movb $3, %al
       jmp vowel_or_consonant_final
       
    vowel_or_consonant_CV:
       # CASE CV = CODE 2
-      movl $2, %eax
+      movb $2, %al
       jmp vowel_or_consonant_final
    
    vowel_or_consonant_VC:
       # CASE VC = CODE 1
-      movl $1, %eax
+      movb $1, %al
    
    vowel_or_consonant_final:
       popl %ebp
@@ -395,37 +409,35 @@ encrypt_bits:
    
    # CHECK THE CASE
    movl 12(%ebp), %ecx
-   cmpl $0, %ecx
+   cmpb $0, %cl
    je case_VV
-   cmpl $1, %ecx
-   #je encrypt_bits_final
+   cmpb $1, %cl
    je count_powers
-   cmpl $2, %ecx
-   #je encrypt_bits_final
+   cmpb $2, %cl
    je count_powers
    
-   # IT'S $3, SO THE CASE IS CC, BUT THAT IS THE SAME AS VV.
+   # IT'S 3 (CC), BUT THAT IS THE SAME AS VV.
    # LET THE CC CASE GO FORWARDS TO CASE_VV.
    
    case_VV:
-      # VV => stringBits & keyBits
-      movl 8(%ebp), %ecx 
-      decl %ecx
+      # VV => stringBits = (stringBits & keyBits)
+      movw 8(%ebp), %cx
+      decw %cx
       movl 8(%ebp), %edx
-      subl $7, %edx
+      subw $7, %dx
       
       case_VV_for:
-         cmpl %edx, %ecx
+         cmpw %dx, %cx
          jl encrypt_bits_final
          
          xorl %eax, %eax
          movb (%esi, %ecx, 1), %al
          xorl %ebx, %ebx
          movb (%edi, %ecx, 1), %bl
-         and %ebx, %eax
+         andb %bl, %al
          movb %al, (%esi, %ecx, 1)
          
-         decl %ecx
+         decw %cx
          jmp case_VV_for
       
    count_powers:
@@ -433,11 +445,11 @@ encrypt_bits:
       # USE %ECX TO COUNT THE NUMBER OF 1'S
       movl 20(%ebp), %ebx # STORE THE KEY CHAR
       movl $32, m
-      movl $1, %ecx
-      subl $64, %ebx
+      movw $1, %cx
+      subb $64, %bl
       
       count_powers_for:
-         cmpl $0, m
+         cmpb $0, m
          je count_powers_final
          
          subl m, %ebx
@@ -445,11 +457,11 @@ encrypt_bits:
          jge count_powers_for_cont
          
          addl m, %ebx
-         decl %ecx
+         decb %cl
          
          count_powers_for_cont:
-            incl %ecx
-            shrl $1, m
+            incb %cl
+            shrb $1, m
             jmp count_powers_for
       
       count_powers_final:
@@ -459,27 +471,27 @@ encrypt_bits:
       # CV (CASE 2) => stringBits - count(0 in keyBits)
       movl 16(%ebp), %eax # GET THE TEXT LETTER
       movl 12(%ebp), %edx # GET THE CASE CODE
-      cmpl $1, %edx
+      cmpb $1, %dl
       je case_VC
       
       case_CV: # CASE 2, SUBTRACT NUMBER OF 0's
-         subl $7, %eax
-         addl %ecx, %eax
+         subb $7, %al
+         addb %cl, %al
          jmp pre_conversion
          
       case_VC: # CASE 1, SUBTRACT NUMBER OF 1'S
-         subl %ecx, %eax
+         subb %cl, %al
          
       pre_conversion:
-         movl 8(%ebp), %edx
-         movl 8(%ebp), %ecx
-         subl $7, %ecx
+         movw 8(%ebp), %dx
+         movw 8(%ebp), %cx
+         subw $7, %cx
          movl $64, m
       
       # %EAX HAS THE CURRENT VALUE TO BE CONVERTED TO BINARY
       # STORE THE BITS IN %ESI
       convert_to_binary:
-         cmpl %ecx, %edx
+         cmpw %cx, %dx
          je encrypt_bits_final
          
          subl m, %eax
@@ -495,12 +507,11 @@ encrypt_bits:
             movb $1, (%esi, %ecx, 1)
             
          convert_to_binary_cont:
-            shrl $1, m
-            incl %ecx
+            shrb $1, m
+            incw %cx
             jmp convert_to_binary
         
    encrypt_bits_final:
-      #movl 8(%ebp), %eax
       popl %ebp
       ret
    
@@ -509,25 +520,25 @@ powers_of_2:
    movl %esp, %ebp
    
    # 8(%ebp) = CURRENT SIZE OF THE BITS ARRAY
-   # 12(%ebp) = CURRENT CASE CODE (PUSHED ONLY TO SAVE ITS VALUE)
+   # 12(%ebp) = CURRENT CASE CODE (WON'T NEED IT, PUSHED ONLY TO SAVE ITS VALUE)
    # 16(%ebp) = CURRENT TEXT CHARACTER
    # 20(%ebp) = CURRENT KEY CHARACTER
    
    movl 8(%ebp), %ecx
    movl %ecx, %edx
-   addl $7, %edx
+   addw $7, %dx
    movl 16(%ebp), %eax
    movl 20(%ebp), %ebx
    
    movb $1, (%esi, %ecx, 1)
    movb $1, (%edi, %ecx, 1)
-   incl %ecx
-   subl $64, %eax
-   subl $64, %ebx
+   incw %cx
+   subb $64, %al
+   subb $64, %bl
    movl $32, m
    
    powers_of_2_for:
-      cmpl %ecx, %edx
+      cmpw %cx, %dx
       je powers_of_2_final
       
       subl m, %eax
@@ -553,14 +564,18 @@ powers_of_2:
          movb $0, (%edi, %ecx, 1)
          
       powers_of_2_for_final:
-         incl %ecx
-         shrl $1, m
+         incw %cx
+         shrb $1, m
          jmp powers_of_2_for
    
    powers_of_2_final:
-      movl %ecx, %eax
+      movw %cx, %ax
       popl %ebp
       ret
+      
+# INPUT = 1 => PERFORM DECRYPTION
+# (todo)
+main_decrypt:
    
 exit:   
    movl $1, %eax
